c29d723ffeb3b41db88aafdcb3aa306dd7ea55d5
deep005 <guhadeepshanker@gmail.com>
2017-10-30 08:04:15 +0530

    initilizing the constructor

---
+++ binarytree.js
@@ -0,0 +1,14 @@
+(function(){
+    let BinaryTree = function(){
+        this.root = null;
+    };
+    let BinaryNode = function(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    };
+
+    BinaryTree.prototype.insert = function(){
+
+    }
+}());
