f1bcfd11f92a3cb46d20a918994e43c136a9094e
deep005 <guhadeepshanker@gmail.com>
2017-11-02 06:22:49 +0530

    adding findParent

--- maxHeap.js
+++ maxHeap.js
@@ -1,7 +1,14 @@
 (function (){
 
-    let MaxHeap = function(){
+    let MaxHeap = function() {
         this.items = [];
-        this.size  = -1;
-    }
+        this.size = -1;
+
+    };
+    MaxHeap.prototype.findParent = function(index){
+        return Math.floor((index-1)/2);
+    };
+    MaxHeap.prototype.insert = function(){
+
+    };
 }());
