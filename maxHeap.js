62fed16777bbe27dbd753a78e961c6fda035be26
deep005 <guhadeepshanker@gmail.com>
2017-11-02 06:33:54 +0530

    adding insert method

--- maxHeap.js
+++ maxHeap.js
@@ -8,7 +8,25 @@
     MaxHeap.prototype.findParent = function(index){
         return Math.floor((index-1)/2);
     };
-    MaxHeap.prototype.insert = function(){
+    MaxHeap.prototype.insert = function(val){
+        let currpos = ++this.size,
+            parentIndex = this.findParent(currpos);
+
+        this.items[currpos] = val;
+
+        if(currpos > 0){
+            while(this.items[currpos] > this.items[parentIndex] ){
+                this.items[currpos] = this.items[parentIndex];
+                this.items[parentIndex] = val;
+
+                currpos = parentIndex;
+                parentIndex = this.findParent(currpos);
+
+            }
+        }
+        return currpos;
+    };
+    MaxHeap.prototype.delete = function(){
 
     };
 }());
